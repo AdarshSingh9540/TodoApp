f06b600209f779bfcbf55e8507712723bfc62c18
Adarsh <adarsh9540984202@gmail.com>
2024-04-01 14:41:37 +0530

    frontend input is added

--- frontendPart/src/App.tsx
+++ frontendPart/src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState<string | null>(null);
+  const [description, setDescription] = useState<string | null>(null);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+    console.log('Title:', e.target.value); // Print title in console
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
   return (
-    <div className="flex flex-col ">
-     <input type="text " placeholder='Enter title' className='border border-black w-2/12 p-2 m-2'  />
+    <div className="flex flex-col">
+    
+      <input
+        type="text"
+        placeholder="Enter title"
+        className="border border-black w-2/12 p-2 m-2"
+        onChange={handleTitleChange} 
+      />
 
-     <input type="text" placeholder='Enter description' className='border border-black w-2/12 p-2 m-2'  />
-
+     
+      <input
+        type="text"
+        placeholder="Enter description"
+        className="border border-black w-2/12 p-2 m-2"
+        onChange={handleDescriptionChange} 
+      />
     </div>
   );
 }
