d19d3b6b0fbc36e49b0562eccac8fb8a56aa450f
Adarsh <adarsh9540984202@gmail.com>
2024-04-06 09:40:03 +0530

    add the changes

--- frontendPart/src/App.tsx
+++ frontendPart/src/App.tsx
@@ -4,28 +4,21 @@ import { CreateTodo } from './components/createTodo';
 import { Todos } from './components/Todos';
 import { ShoWTodos } from './components/ShowTodos';
 
+
 interface Todo {
-  id: string;
+  _id: string;
   title: string;
   description: string;
 }
-
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+  const [todos, setTodos] = useState([]); 
 
   const fetchTodos = async () => {
     try {
       const response = await fetch("http://localhost:3000/todos");
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const json = await response.json();
+      console.log(response)
+      const json = await response.json(); 
       setTodos(json.todos);
-      console.log(json.todos);
     } catch (error) {
       console.error('Error fetching todos:', error);
     }
@@ -33,20 +26,21 @@ function App() {
 
   return (
     <div>
-      <CreateTodo />
+      <h1>Todo List</h1>
+      <CreateTodo /> 
       <Todos todos={todos} />
       <div>
-        {todos.length > 0 ? (
-          todos.map(todo => (
-            <ShoWTodos
-              key={todo.id}
-              title={todo.title}
-              description={todo.description}
-            />
-          ))
-        ) : (
-          <p>No todos available</p>
-        )}
+
+        {todos.map((todo: any) => (
+          <ShoWTodos
+            key={todo._id}
+            title={todo.title}
+            description={todo.description}
+          
+          />
+        ))}
+
+        <button onClick={fetchTodos} className='bg-green-500 p-2'>Show TOdo</button>
       </div>
     </div>
   );
--- frontendPart/src/components/ShowTodos.tsx
+++ frontendPart/src/components/ShowTodos.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export function ShoWTodos({ title, description }) {
+    console.log(title)
     return (
         <div>
             <h3>Title: {title}</h3>
